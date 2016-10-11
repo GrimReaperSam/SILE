Fixing ranksum formula to start from index 1 and not 0 Fixing z formula to divide by the std and not the variance

src/characteristics/ranking.py
@@ -22,14 +22,13 @@ def ranksum_characteristic(c_pos, c_neg):
     sorted_characteristics = np.sort(concatenated_characteristics)
 
     positive_indices, = np.where(np.in1d(sorted_characteristics, c_pos))
-    ranksum_t = np.sum(positive_indices)
+    ranksum_t = np.sum(positive_indices) + np.size(positive_indices)  # Adding the size to account for +1 for each index
 
     positive_count = len(c_pos)
     negative_count = len(c_neg)
     expected_mean = positive_count * (positive_count + negative_count + 1) / 2
     expected_variance = positive_count * negative_count * (positive_count + negative_count + 1) / 12
-
-    z = (ranksum_t - expected_mean) / expected_variance
+    z = (ranksum_t - expected_mean) / np.sqrt(expected_variance)
 
     adjusted_t = REFERENCE_SAMPLE_SIZE / positive_count * ranksum_t
     adjusted_mean = REFERENCE_SAMPLE_SIZE / positive_count * expected_mean