dc15c6d189d99c815a431bcc3850bc6e92a07817
GrimReaperSam <fayezlahoud@gmail.com>
2016-10-24 23:52:25 +0200

    Updating paths for server

--- src/filesystem/config_paths.py
+++ src/filesystem/config_paths.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
 # BASE PATH
-BASE_PATH = Path('/datafast/alindner/mirflickr/DB')
+BASE_PATH = Path('/datafast/alindner/mirflickr/')
+DB_PATH = Path('DB/')
 
 # MIR FLICKER BASE FILE
-FLICKER_BASE_TXT = BASE_PATH / './id_kw_mirflickr.txt'
+FLICKER_BASE_TXT = BASE_PATH / 'id_kw_mirflickr.txt'
 FLICKER_PANDAS_STRUCTURE = 'mirflickr_pd.pkl'
 
 # DB PATHES
-RGB_IMAGES = BASE_PATH / 'photos/1024'
-LAB_IMAGES = BASE_PATH / 'photos/cielab'
+RGB_IMAGES = BASE_PATH / DB_PATH / 'photos/1024'
+LAB_IMAGES = BASE_PATH / DB_PATH / 'photos/cielab'
