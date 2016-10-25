e0593895d50f80d59f085ff27e3dfd7964e2cae1
GrimReaperSam <fayezlahoud@gmail.com>
2016-10-25 15:45:51 +0200

    Fixing method signatures

--- src/filesystem/config_paths.py
+++ src/filesystem/config_paths.py
@@ -13,16 +13,16 @@ RGB_IMAGES = BASE_PATH / DB_PATH / 'photos/1024'
 LAB_IMAGES = BASE_PATH / DB_PATH / 'photos/cielab'
 
 
-def path_from_id(self, image_id):
-    return Path(self.__get_prefix(image_id)) / ("%s.jpg" % image_id)
+def path_from_id(image_id):
+    return Path(__get_prefix(image_id)) / ("%s.jpg" % image_id)
 
 
-def rgb_from_id(self, image_id):
-    return Path(RGB_IMAGES) / self.path_from_id(image_id)
+def rgb_from_id(image_id):
+    return Path(RGB_IMAGES) / path_from_id(image_id)
 
 
-def lab_from_id(self, image_id):
-    return Path(LAB_IMAGES) / self.path_from_id(image_id)
+def lab_from_id(image_id):
+    return Path(LAB_IMAGES) / path_from_id(image_id)
 
 
 def __get_prefix(image_id):
