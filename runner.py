2f310074c907ecd1febd1c2d58e7e473a48acb47
GrimReaperSam <fayezlahoud@gmail.com>
2016-10-29 23:31:38 +0200

    GrayLevel debugging

--- runner.py
+++ runner.py
@@ -7,18 +7,21 @@
 #
 # gray_hist = GrayLevelHistogram()
 #
-# darks = os.listdir('DB/dark')
+# darks = os.listdir('DB/dark')[:2]
+# print(darks)
 # dark_hists = np.zeros((len(darks), *gray_hist.shape))
 # for index, image in enumerate(darks):
 #     dark = imread('DB/dark/' + image)
 #     dark_hists[index] = gray_hist.compute(dark)
 #
-# lights = os.listdir('DB/white')
+# lights = os.listdir('DB/white')[:2]
+# print(lights)
 # light_hists = np.zeros((len(lights), *gray_hist.shape))
 # for index, image in enumerate(lights):
 #     light = imread('DB/white/' + image)
 #     light_hists[index] = gray_hist.compute(light)
-#
+# print(dark_hists)
+# print(light_hists)
 # rsum = ranksum(dark_hists, light_hists)
 # print(delta_z(rsum))
 #
@@ -28,25 +31,7 @@
 #     plt.imshow(rsum, cmap='gray')
 # plt.show()
 #
-
-
-# from src.characteristics.descriptors_calculator import DescriptorsCalculator
 #
-# dc = DescriptorsCalculator()
-#
-# i = os.listdir('DB/dark')
-# images = list(map(lambda n: imread('DB/dark/' + n), i))
-# chars = dc.describe_set(images, 'lightness_layout')
-# print(chars.shape)
-# print(chars[0:5])
-
-# i = imread('DB/dark/8.jpg')
-# dh = LightnessFourier()
-# a = dh.compute(i)
-# plt.imshow(a, cmap='gray')
-# plt.show()
-
-
 # import logging
 # logging.getLogger().setLevel(logging.INFO)
 #
