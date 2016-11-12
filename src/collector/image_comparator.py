10589841a3ed28848b5f8f7975e8179eac84bc7e
GrimReaperSam <fayezlahoud@gmail.com>
2016-11-13 00:53:45 +0100

    Thresholding to decide if we enhance the image or not

--- src/collector/image_comparator.py
+++ src/collector/image_comparator.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from ..shared import *
+
 
 class ImageComparator:
     def __init__(self, z_collector):
@@ -9,10 +11,14 @@ class ImageComparator:
         z_collection = self.z_collector.collect(keyword)
         for key in z_collection.descriptors:
             description_data = z_collection.descriptors[key]
-            delta = self._compare_descriptor(image, key, description_data)
-            z_times_delta = description_data.descriptor * delta
-            print(delta)
-            print(z_times_delta)
+            if description_data.delta_z > Z_VALUE_THRESHOLD:
+                delta, image_description = self._compare_descriptor(image, key, description_data)
+                # Delta is positive so we need the highest difference to see if it's worth checking this descriptor
+                if delta.max() > DELTA_THRESHOLD:
+                    z_times_delta = description_data.descriptor * delta
+
+                    # TODO below
+                    #enhance_image(description_data, image_description, image)
 
     def _compare_descriptor(self, image, key, description_data):
         descriptor = description_data.descriptor
