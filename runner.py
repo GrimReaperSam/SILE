4c8fdaa784e7552c3564dc29c88c0b9295a778e3
GrimReaperSam <fayezlahoud@gmail.com>
2016-11-13 20:09:09 +0100

    Trying out image_comparator

--- runner.py
+++ runner.py
@@ -1,4 +1,8 @@
 import logging
+from pathlib import Path
+
+from skimage.io import imsave
+
 from src.filesystem.providers import *
 from src.collector import *
 
@@ -8,5 +12,11 @@ a = ZCollector(MyDescriptorProvider(), MyZProvider())
 hist = a.collect('night')
 print(hist)
 
-# b = ImageComparator(a)
-# b.compare(12, 'night')
+b = ImageComparator(a)
+
+parent_path = Path('/data/lahoud/DB/examples')
+parent_path.mkdir(exist_ok=True, parents=True)
+
+for i in range(12):
+    result = b.compare(i, 'night', 2)
+    imsave(str(parent_path / 'result-%s.jpg' % i), result)
--- src/collector/image_comparator.py
+++ src/collector/image_comparator.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from scipy.interpolate import interp1d
+
 from ..shared import *
 
 
@@ -7,18 +9,13 @@ class ImageComparator:
     def __init__(self, z_collector):
         self.z_collector = z_collector
 
-    def compare(self, image, keyword):
+    def compare(self, image, keyword, s):
         z_collection = self.z_collector.collect(keyword)
-        for key in z_collection.descriptors:
+        for key in ['gray_hist']:
             description_data = z_collection.descriptors[key]
             if description_data.delta_z > Z_VALUE_THRESHOLD:
-                delta, image_description = self._compare_descriptor(image, key, description_data)
-                # Delta is positive so we need the highest difference to see if it's worth checking this descriptor
-                if delta.max() > DELTA_THRESHOLD:
-                    z_times_delta = description_data.descriptor * delta
-
-                    # TODO below
-                    #enhance_image(description_data, image_description, image)
+                return self.enhance_image(image, key, description_data, s)
+        return None
 
     def _compare_descriptor(self, image, key, description_data):
         descriptor = description_data.descriptor
@@ -26,6 +23,40 @@ class ImageComparator:
         image_description = self.z_collector.descriptor_calculator.describe_image(image, key)
 
         less_than = descriptor >= 0
-        delta[less_than] = np.maximum(np.zeros(descriptor.shape), description_data.quantiles[2] - image_description)[less_than]
-        delta[~less_than] = np.maximum(np.zeros(descriptor.shape), image_description - description_data.quantiles[0])[~less_than]
+        delta[less_than] = np.maximum(np.zeros(descriptor.shape), description_data.quantiles[2] - image_description)[
+            less_than]
+        delta[~less_than] = np.maximum(np.zeros(descriptor.shape), image_description - description_data.quantiles[0])[
+            ~less_than]
         return delta
+
+    def enhance_image(self, image, key, description_data, s):
+        delta, image_description = self._compare_descriptor(image, key, description_data)
+        # Delta is positive so we need the highest difference to see if it's worth checking this descriptor
+        if delta.max() > DELTA_THRESHOLD:
+            z_delta = description_data.descriptor * delta
+            result = transfer(image, z_delta, s)
+        return None
+
+
+def transfer(image, z_delta, s):
+    transfer_function = interp1d(*make_transfer(z_delta, s), fill_value='extrapolate')
+    return transfer_function(image / 255).astype(np.uint8)
+
+
+def make_transfer(s, zd):
+    s_max = 5
+
+    f_derivative = interp1d(np.linspace(1 / 32, 31 / 32, 16), s * zd, fill_value='extrapolate')
+
+    x2 = np.linspace(0, 1, 255)
+    slopes = f_derivative(x2)
+    slopes[slopes >= 0] = 1 / (1 + s * slopes[slopes >= 0])
+    slopes[slopes < 0] = 1 + np.abs(slopes[slopes < 0])
+
+    slopes[slopes > s_max] = s_max
+    slopes[slopes < 1 / s_max] = 1 / s_max
+
+    mmap = np.cumsum(slopes)
+    mmap -= mmap.min()
+    mmap = mmap / mmap.max() * 255
+    return x2, mmap
