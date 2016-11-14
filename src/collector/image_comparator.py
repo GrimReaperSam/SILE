efd06ceec98574a6b51c635a8ac816bf320e81a6
GrimReaperSam <fayezlahoud@gmail.com>
2016-11-14 11:58:59 +0100

    Adding lab_to_rgb and lch_to_lab

--- src/collector/image_comparator.py
+++ src/collector/image_comparator.py
@@ -23,7 +23,7 @@ def enhance_image(image, key, description_data, s):
     # Delta is positive so we need the highest difference to see if it's worth checking this descriptor
     if delta.max() > DELTA_THRESHOLD:
         z_delta = description_data.descriptor * delta
-        return transfer(image, z_delta, s)
+        return transfer(image, key, z_delta, s)
     return image
 
 
@@ -39,17 +39,35 @@ def compare_descriptor(image, key, description_data):
     return delta, image_description
 
 
-def transfer(image, z_delta, s):
-    transfer_function = interp1d(*make_transfer(s, z_delta), fill_value='extrapolate')
-    return transfer_function(image / 255).astype(np.uint8)
+def transfer(image, key, z_delta, s):
+    transfer_function = interp1d(*make_transfer(key, s, z_delta), fill_value='extrapolate')
+    if key == 'gray_hist':
+        return transfer_function(image / 255).astype(np.uint8)
+    elif key == 'chroma_hist':
+        lch = lab_to_lch(rgb_to_lab(image))
+        lch[..., 1] = transfer_function(lch[..., 1] / 80)
+        return lab_to_rgb(lch_to_lab(lch))
+    elif key == 'hue_angle_hist':
+        lch = lab_to_lch(rgb_to_lab(image))
+        lch[..., 2] = transfer_function(lch[..., 2] / 360)
+        return lab_to_rgb(lch_to_lab(lch))
 
 
-def make_transfer(s, zd):
+def make_transfer(key, s, zd):
     s_max = 5
 
-    f_derivative = interp1d(np.linspace(1 / 32, 31 / 32, 16), s * zd, fill_value='extrapolate')
+    if key == 'gray_hist':
+        bin_centers = np.linspace(1 / 32, 31 / 32, 16)
+        x2 = np.linspace(0, 1, 255)
+    elif key == 'chroma_hist':
+        bin_centers = np.linspace(1 / 32, 31 / 32, 16)
+        x2 = np.linspace(0, 1, 80)
+    elif key == 'hue_angle_hist':
+        bin_centers = np.linspace(1 / 32, 31 / 32, 16)
+        x2 = np.linspace(0, 1, 360)
 
-    x2 = np.linspace(0, 1, 255)
+    f_derivative = interp1d(bin_centers, s * zd, fill_value='extrapolate')
+
     slopes = f_derivative(x2)
     slopes[slopes >= 0] = 1 / (1 + s * slopes[slopes >= 0])
     slopes[slopes < 0] = 1 + np.abs(slopes[slopes < 0])
@@ -59,5 +77,11 @@ def make_transfer(s, zd):
 
     mmap = np.cumsum(slopes)
     mmap -= mmap.min()
-    mmap = mmap / mmap.max() * 255
-    return x2, mmap
+    mmap = mmap / mmap.max()
+
+    if key == 'gray_hist':
+        return x2, mmap * 255
+    elif key == 'chroma_hist':
+        return x2, mmap * 80
+    elif key == 'hue_angle_hist':
+        return x2, mmap * 360
