9ec10ad44e2736e037b509ba37505c5c237609e2
GrimReaperSam <fayezlahoud@gmail.com>
2016-12-16 20:29:41 +0100

    Moving masks

--- copy-images.py
+++ copy-images.py
@@ -6,14 +6,32 @@ from src.filesystem import FlickerDB
 from src.config_paths import rgb_from_id
 from pathlib import Path
 
+
+def __get_prefix(image_id):
+    if image_id > 9:
+        return str(image_id)[-2:]
+    else:
+        return "0%s" % str(image_id)[-1]
+
+
 if __name__ == '__main__':
-    db = FlickerDB()
+    keywords = ['banana', 'boeing', 'cheese', 'dessert', 'ferrari', 'lily',
+                'orchid', 'strawberry', 'sunflower', 'tulip']
 
-    pos, _ = db.ids_by_tag(sys.argv[1])
+    base_path = Path('/data/bjin/Mirflickr/masks')
 
-    save_path = Path('/data/lahoud/DB/images/') / sys.argv[1]
+    save_path = Path('/data/lahoud/DB/masks')
     save_path.mkdir(exist_ok=True, parents=True)
 
-    for p in pos:
-        img_path = rgb_from_id(p)
-        copy2(str(img_path), str(save_path))
+    for kw in keywords:
+        origin = base_path / kw
+
+        for image in origin.iterdir():
+            name = image.stem
+            name_int = int(name)
+
+            prefix = __get_prefix(name_int)
+            save_in = save_path / prefix
+            save_in.mkdir(exist_ok=True, parents=True)
+
+            copy2(str(image), str(save_in / image.name))
